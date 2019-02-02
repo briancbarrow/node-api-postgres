ef2ef49b324c08b43ee8f0338cf21336817ca756
Brian Barrow <briancbarrow@gmail.com>
2019-02-02 09:44:44 -0700

    added api functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import * as db from "./queries";
 
 const app = express();
 const port = 3000;
@@ -14,6 +15,12 @@ app.use(
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgress API" });
 });
+
+app.get("/users", db.getUsers);
+app.get("/user/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
